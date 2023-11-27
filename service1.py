0cd44a4af50b463565f0b660dc52fe2100955078
ma <maa350@mail.aub.edu>
2023-11-27 13:51:30 +0200

    created the first service:costumers

--- service1.py
+++ service1.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from database import *
+from database1 import *
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
